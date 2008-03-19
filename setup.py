f202adb911c403f519f32c95e54cc72a4b7e34a2
Loui Chang <louipc.ist@gmail.com>
2008-03-19 11:28:53 -0400

    Tweak setup.py Signed-off-by: Loui Chang <louipc.ist@gmail.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 NAME="aurbuild"
 VERSION="1.8.3"
-DESC="An utility to build and install packages from Archlinux User Repository"
+DESC="An utility to build and install packages from Arch Linux User Repository"
+LICENSE="GPL"
+URL="http://aurbuild.berlios.de"
 AUTHOR="Tyler Gates, Loui Chang"
 EMAIL="TGates81@gmail.com, louipc.ist@gmail.com"
-LICENSE="GPL"
 
 from distutils.core import setup
 
@@ -13,9 +14,10 @@ DATAFILES = [('/usr/man/man1', ['aurbuild.1']),
 		('/usr/share/aurbuild-'+VERSION,
 		['BUGS', 'COPYING', 'README', 'AUTHORS', 'CHANGELOG'])]
 
-setup(name="aurbuild",
+setup(name=NAME,
 	version=VERSION,
 	description=DESC,
+	url=URL,
 	author=AUTHOR,
 	author_email=EMAIL,
 	license=LICENSE,
