3acb139f830127444f842b279949777d8aad169b
Loui Chang <louipc.ist@gmail.com>
2008-07-25 03:08:10 -0400

    Change setup.py formatting.
    Signed-off-by: Loui Chang <louipc.ist@gmail.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 
-NAME="aurbuild"
-VERSION="1.8.4"
-DESC="An utility to build and install packages from Arch Linux User Repository"
-LICENSE="GPL"
-URL="http://aurbuild.berlios.de"
-AUTHOR="Tyler Gates, Loui Chang"
-EMAIL="TGates81@gmail.com, louipc.ist@gmail.com"
-
 from distutils.core import setup
 
+NAME = "aurbuild"
+VERSION = "1.8.4"
+DESC = "An utility to build and install packages from Arch Linux User Repository"
+LICENSE = "GPL"
+URL = "http://aurbuild.berlios.de"
+AUTHOR = "Tyler Gates, Loui Chang"
+EMAIL = "TGates81@gmail.com, louipc.ist@gmail.com"
+
 DATAFILES = [('/usr/man/man1', ['aurbuild.1']),
-		('/usr/share/aurbuild-'+VERSION,
+		('/usr/share/aurbuild-' + VERSION,
 		['BUGS', 'COPYING', 'README', 'AUTHORS'])]
 
-setup(name=NAME,
-	version=VERSION,
-	description=DESC,
-	url=URL,
-	author=AUTHOR,
-	author_email=EMAIL,
-	license=LICENSE,
-	platforms="linux2",
-	packages=['aurbuild'],
-	package_dir={'aurbuild':'src'},
-	scripts=['scripts/aurbuild'], 
+setup(
+	name = NAME,
+	version = VERSION,
+	description = DESC,
+	url = URL,
+	author = AUTHOR,
+	author_email = EMAIL,
+	license = LICENSE,
+	platforms = "linux2",
+	packages = ['aurbuild'],
+	package_dir = {'aurbuild':'src'},
+	scripts = ['scripts/aurbuild'], 
 	data_files = DATAFILES)
-
 
