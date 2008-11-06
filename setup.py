100ab00900826ae6976f9ecd32b7d698e8aacb76
Loui Chang <louipc.ist@gmail.com>
2008-11-06 17:25:18 -0500

    Install the man page in /usr/share/man.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "http://aurbuild.berlios.de"
 AUTHOR = "Tyler Gates, Loui Chang"
 EMAIL = "TGates81@gmail.com, louipc.ist@gmail.com"
 
-DATAFILES = [('/usr/man/man1', ['aurbuild.1']),
+DATAFILES = [('/usr/share/man/man1', ['aurbuild.1']),
 		('/usr/share/aurbuild-' + VERSION,
 		['BUGS', 'COPYING', 'README', 'AUTHORS'])]
 
