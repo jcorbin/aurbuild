5d6a569feb096f73cbef9d526d749bb0ef03e5cf
Loui Chang <louipc.ist@gmail.com>
2009-03-18 02:36:31 -0400

    Bump version to 1.8.6
    Signed-off-by: Loui Chang <louipc.ist@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 NAME = "aurbuild"
-VERSION = "1.8.5"
+VERSION = "1.8.6"
 DESC = "An utility to build and install packages from Arch Linux User Repository"
 LICENSE = "GPL"
 URL = "http://aurbuild.berlios.de"
